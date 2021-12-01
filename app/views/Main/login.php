ee9e771512729839b7a3d50f0a3b064564e36c1e
Cryotechnic <cryogen65@gmail.com>
2021-12-01 15:40:08 -0500

    styled login

--- app/views/Main/login.php
+++ app/views/Main/login.php
@@ -2,7 +2,8 @@
 	<head>
 		<title>Login</title>
 		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+   	    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+		<link rel="stylesheet" type="text/css" href="/css/login.css">
 	</head>
 	<body>
 	<?php 
@@ -10,11 +11,38 @@
 			echo $data;
 		}
 	?>
-	Login to an existing user or <a href="/Main/register">Register</a> a new user
-		<form action='' method='post'>
-			Username: <input type='text' name='username' /><br>
-			Password: <input type='password' name='password' /><br>
-			<input type='submit' name="action" value='Login' />
-		</form>
+		<div class="global-container">
+	<div class="card login-form">
+	<div class="card-body">
+		<h3 class="card-title text-center">Log in to Codepen</h3>
+		<div class="card-text">
+		<?php 
+			if($data != null) {
+				echo $data;
+			}
+		?>
+			<!--
+			<div class="alert alert-danger alert-dismissible fade show" role="alert">Incorrect username or password.</div> -->
+			<form action='' method='post'>
+				<!-- to error: add class "has-danger" -->
+				<div class="form-group">
+					<label for="exampleInputEmail1">Email address</label>
+					<input type="text" class="form-control form-control-sm" id="exampleInputEmail1" aria-describedby="emailHelp" name="username">
+				</div>
+				<div class="form-group">
+					<label for="exampleInputPassword1">Password</label>
+					<input type="password" class="form-control form-control-sm" id="exampleInputPassword1" name="password">
+				</div>
+				<button type="submit" class="btn btn-primary btn-block">Sign in</button>
+				
+				<div class="sign-up">
+					Don't have an account? <a href="#">Create One</a>
+				</div>
+			</form>
+		</div>
+	</div>
+</div>
+</div>
+
 	</body>
 </html>
